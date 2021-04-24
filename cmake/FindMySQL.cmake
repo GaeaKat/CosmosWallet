b926fe00278666c70a0a0890feb0ce1580179a82
Katrina Knight <kat.knight@newgaea.net>
2021-04-24 19:13:19 +0100

    Fixed MySQL

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -4,7 +4,7 @@ INCLUDE (FindPackageHandleStandardArgs)
 SET(MYSQL_ROOT_DIR  /usr/local/mysql/connector-c++-/)
 # Re-use the previous path:
 FIND_PATH (MYSQL_INCLUDE_DIR
-        NAMES mysqlx/xapi.h
+        NAMES mysql/jdbc.h
         HINTS ${MYSQL_ROOT_DIR}
         PATH_SUFFIXES include
         DOC "MySQL include directory")
@@ -13,15 +13,18 @@ FIND_LIBRARY (MYSQL_LIBRARY
         HINTS ${MYSQL_ROOT_DIR}
         PATH_SUFFIXES lib64/debug
         DOC "MySQL debug library")
+
+SET( MYSQL_OTHER_LIBRARY ${MYSQL_ROOT_DIR}lib64/debug/libmysqlcppconn.so)
+
 #FIND_LIBRARY (MYSQL_LIBRARY_RELEASE
 #        NAMES libmysqlcppconn8.so
 #        HINTS ${MYSQL_ROOT_DIR}
 #        PATH_SUFFIXES lib64
 #        DOC "MySQL release library")
 
-
+message("${MYSQL_OTHER_LIBRARY} - other one")
 SET (MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDE_DIR})
-SET (MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+SET (MYSQL_LIBRARIES ${MYSQL_LIBRARY} ${MYSQL_OTHER_LIBRARY})
 
 MARK_AS_ADVANCED (MYSQL_INCLUDE_DIR MYSQL_LIBRARY)
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "include/matterpool_timechain.h"
 #include "include/mongodb_db.h"
 
+#include <mysql/jdbc.h>
+
 int main() {
     using namespace Cosmos;
     std::cout << "Hello, World!" << std::endl;
@@ -14,5 +16,47 @@ int main() {
     auto tmp2=timechain.transaction(digest);
     std::cout << tmp.Value.Header << " tmp " << tmp << std::endl;
     std::cout << tmp2.Value.Header << " tmp2 " << tmp2 << std::endl;
+
+    string url = "tcp://url-no-port";
+    string user="username";
+    string password="password";
+    string db="dbaname";
+    try {
+        sql::Driver * driver = sql::mysql::get_driver_instance();
+        std::cout << "Creating session on " << url << std::endl;
+        std::unique_ptr<sql::Connection> con(driver->connect(url,user,password));
+        con->setSchema(db);
+        std::unique_ptr<sql::Statement> stmt(con->createStatement());
+        std::unique_ptr< sql::ResultSet > res(stmt->executeQuery("SHOW TABLES"));
+        std::cout << "Tables in DB: " <<std::endl;
+        while(res->next()) {
+            std::cout << res->getString(1) << std::endl;
+        }
+
+    }
+    catch (sql::SQLException &e)
+    {
+        /*
+          The JDBC API throws three different exceptions:
+
+        - sql::MethodNotImplementedException (derived from sql::SQLException)
+        - sql::InvalidArgumentException (derived from sql::SQLException)
+        - sql::SQLException (derived from std::runtime_error)
+        */
+
+        std::cout << "# ERR: SQLException in " << __FILE__;
+        std::cout << "(" << "EXAMPLE_FUNCTION" << ") on line " << __LINE__ << std::endl;
+
+        /* Use what() (derived from std::runtime_error) to fetch the error message */
+
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+
+
     return 0;
 }
