2d8925d4c013972241d10e1c96b2dffe2a33a00c
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2021-05-03 22:43:08 -0500

    Started on postgres connection

--- include/sql.h
+++ include/sql.h
@@ -9,41 +9,101 @@
 #include <mysql/jdbc.h>
 #include <pqxx/pqxx>
 
-namespace Cosmos {
+namespace Cosmos::SQL {
     
-    struct sql_session final {
+    enum type {
+        MYSQL,
+        POSTGRES
+    };
     
-        struct parameters {
-            string URL;
-            string User;
-            string Password;
-            string DB;
-        };
+    struct parameters {
+        string URL;
+        string User;
+        string Password;
+        string DB;
+    };
+    
+    struct description {
+        
+        string Name;
+        string Type;
+        string Null;
+        string Key;
+        string Default;
+        string Extra;
+        
+    };
+    
+    struct session {
+        
+        virtual list<string> show_tables() = 0;
+        virtual list<description> describe(const string& table) = 0;
+        virtual int64_t rows_in_table(const string& table) = 0;
+        
+        virtual ~session() {}
+        
+    };
+    
+    struct mysql final : session {
         
         std::unique_ptr<sql::ResultSet> execute(const string &query) {
             return std::unique_ptr<sql::ResultSet>{Connection->createStatement()->executeQuery(query)};
         }
         
-        static sql_session connect_mysql(const parameters &p) {
-            sql_session x{sql::mysql::get_driver_instance()->connect(p.URL, p.User, p.Password)};
-            x.Connection->setSchema(p.DB);
+        static mysql *connect(const parameters &p) {
+            mysql* x = new mysql{sql::mysql::get_driver_instance()->connect(p.URL, p.User, p.Password)};
+            x->Connection->setSchema(p.DB);
             return x;
         }
         
-        // TODO
-        static sql_session postgres_connect(const parameters &p);
-        
-        ~sql_session() {
+        ~mysql() {
             delete Connection;
         }
         
-        list<string> show_tables();
+        list<string> show_tables() override;
+        list<description> describe(const string& table) override;
+        int64_t rows_in_table(const string& table) override;
         
     private:
         sql::Connection *Connection;
-        sql_session(sql::Connection *conn) : Connection{conn} {}
+        mysql(sql::Connection *conn) : Connection{conn} {}
     };
     
+    struct postgres final : session {
+        
+        pqxx::result execute(const string &query) {
+            return pqxx::nontransaction(Connection).exec(query);
+        }
+        
+        list<string> show_tables() override;
+        
+        list<description> describe(const string& table) override;
+        int64_t rows_in_table(const string& table) override;
+        
+        static postgres *connect(const parameters &p) {
+            return new postgres{p};
+        }
+        
+        ~postgres() {
+            Connection.disconnect();
+        }
+        
+    private:
+        pqxx::connection Connection;
+        string DBName;
+        postgres(const parameters &p) : Connection{
+            string{} + "dbname=" + p.DB + " user=" + p.User + " password=" + p.Password + " hostaddr=" + p.URL + " port = 5432"}, DBName{p.DB} {}
+    };
+    
+    std::ostream inline &operator<<(std::ostream &o, const description& d) {
+        return o << "[\"" 
+            << d.Name << "\", \"" 
+            << d.Type << "\", \"" 
+            << d.Null << "\", \"" 
+            << d.Key << "\", \"" 
+            << d.Default << /*"\", \"" 
+            << d.Extra <<*/ "\"]";
+    }
     
 }
 
--- src/sql.cpp
+++ src/sql.cpp
@@ -1,9 +1,9 @@
 
 #include "sql.h"
 
-namespace Cosmos {
+namespace Cosmos::SQL {
     
-    list<string> sql_session::show_tables() {
+    list<string> mysql::show_tables() {
         
         std::unique_ptr< sql::ResultSet > result = execute("SHOW TABLES");
         
@@ -14,6 +14,83 @@ namespace Cosmos {
         
         return tables;
         
+    }
+    
+    list<description> mysql::describe(const string& table) {
+        
+        string query = string{"DESCRIBE "} + table;
+        
+        std::unique_ptr< sql::ResultSet > result = execute(query);
+        
+        list<description> rows;
+        
+        while(result->next()) {
+            rows = rows << description{
+                result->getString(1), 
+                result->getString(2), 
+                result->getString(3), 
+                result->getString(4), 
+                result->getString(5), 
+                result->getString(6)};
+        } 
+        
+        return rows;
+        
+    }
+        
+    list<string> postgres::show_tables() {
+        pqxx::result result(pqxx::nontransaction(Connection).exec("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'"));
+        
+        list<string> tables;
+        
+        for (pqxx::result::const_iterator c = result.begin(); c != result.end(); ++c) {
+            tables = tables << c[1].as<std::string>();
+        }
+        
+        return tables;
+    }
+        
+    list<description> postgres::describe(const string& table) {
+        string query = string{"select column_name, data_type, character_maximum_length, column_default, is_nullable from INFORMATION_SCHEMA.COLUMNS where table_name = \'"} + table + "\'";
+        
+        pqxx::result result(pqxx::nontransaction(Connection).exec(query));
+        
+        list<description> rows;
+        
+        for (pqxx::result::const_iterator c = result.begin(); c != result.end(); ++c) {
+            
+            rows = rows << description{
+                c[0].is_null() ? string{} : c[0].as<string>(), 
+                c[1].is_null() ? string{} : c[1].as<string>(), 
+                c[2].is_null() ? string{} : c[2].as<string>(), 
+                c[3].is_null() ? string{} : c[3].as<string>(), 
+                c[4].is_null() ? string{} : c[4].as<string>(), 
+                c[5].is_null() ? string{} : c[5].as<string>()};
+        }
+        
+        return rows;
+    }
+    
+    int64_t mysql::rows_in_table(const string& table) {
+        string query = string{"SELECT COUNT(*) FROM "} + table;
+        
+        std::unique_ptr< sql::ResultSet > result = execute(query);
+        
+        list<description> rows;
+        
+        if(result->next()) return result->getInt(1);
+            
+        return -1;
+        
+    }
+    
+    int64_t postgres::rows_in_table(const string& table) {
+        string query = string{"SELECT COUNT(*) FROM "} + table;
+        
+        // string query = string{"select sum([rows]) from sys.partitions where object_id=object_id('"} + table + "') and index_id in (0,1)";
+        pqxx::result result(pqxx::nontransaction(Connection).exec(query));
+        
+        return result.begin()[1].as<int64_t>();
     }
 
 }
