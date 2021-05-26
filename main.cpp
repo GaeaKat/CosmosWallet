7cd2b1a7d2d54171ba0ea3c4f5ad23d6889721bd
Katrina Knight <kat.knight@newgaea.net>
2021-05-26 20:35:50 +0100

    Made the DB organiser

--- main.cpp
+++ main.cpp
@@ -16,49 +16,9 @@ int main() {
     std::cout << tmp.Value.Header << " tmp " << tmp << std::endl;
     std::cout << tmp2.Value.Header << " tmp2 " << tmp2 << std::endl;
 
-    string url = "tcp://url-no-port";
-    string user="username";
-    string password="password";
-    string db="dbaname";
-    try {
-        
-        sql::Driver * driver = sql::mysql::get_driver_instance();
-        std::cout << "Creating session on " << url << std::endl;
-        std::unique_ptr<sql::Connection> con(driver->connect(url, user, password));
-        con->setSchema(db);
-        std::unique_ptr<sql::Statement> stmt(con->createStatement());
-        std::unique_ptr< sql::ResultSet > res(stmt->executeQuery("SHOW TABLES"));
-        std::cout << "Tables in DB: " <<std::endl;
-        while(res->next()) {
-            std::cout << res->getString(1) << std::endl;
-        }
-
-    }
-    catch (sql::SQLException &e)
-    {
-        /*
-          The JDBC API throws three different exceptions:
-
-        - sql::MethodNotImplementedException (derived from sql::SQLException)
-        - sql::InvalidArgumentException (derived from sql::SQLException)
-        - sql::SQLException (derived from std::runtime_error)
-        */
-
-        std::cout << "# ERR: SQLException in " << __FILE__;
-        std::cout << "(" << "EXAMPLE_FUNCTION" << ") on line " << __LINE__ << std::endl;
-
-        /* Use what() (derived from std::runtime_error) to fetch the error message */
-
-        std::cout << "# ERR: " << e.what();
-        std::cout << " (MySQL error code: " << e.getErrorCode();
-        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
-
-        //return EXIT_FAILURE;
-    }
-
     try {
         pqxx::connection C(
-                "dbname = database user = username password = password hostaddr =ipaddress port = 5432");
+                "dbname = dbname user = user password = password hostaddr =address port = 5432");
         if (C.is_open()) {
             std::cout << "Opened database successfully: " << C.dbname() << std::endl;
         } else {
@@ -66,14 +26,33 @@ int main() {
             return 1;
         }
         //* Create SQL statement *//
-        char *sql = "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND  schemaname != 'information_schema'";
+        char *sql = "select * from \"boost_job\"";
 
         pqxx::nontransaction N(C);
+
         pqxx::result R(N.exec(sql));
         std::cout << "Postgres tables" << std::endl;
+        int count=0;
+        std::vector<std::string> invalids;
         for (pqxx::result::const_iterator c = R.begin(); c != R.end(); ++c) {
-            std::cout << "table: " << c[1].as<std::string>() << std::endl;
+            auto txid=c[0].as<std::string>();
+            bool spent=!c[6].is_null();
+            if(!spent) {
+                auto check=timechain.transaction(Gigamonkey::digest<32>("0x"+txid));
+                if(!check.valid()) {
+                    count++;
+                    invalids.push_back(txid);
+                }
+            }
         }
+        std::cout << "Count: " << count;
+        N.commit();
+        pqxx::work w(C);
+        for(const auto& txid: invalids) {
+            w.exec("DELETE FROM \"boost_job\" WHERE txid = '"+txid+"'");
+        }
+        w.commit();
+
         C.disconnect();
     }catch (const std::exception &e) {
         std::cerr <<"Errored: " << e.what()  << std::endl;
